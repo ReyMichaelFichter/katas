5c8cfa09fc65a17a8fe2eccafe2664eeac525f86
Michael Fichter <michael.fichter@klarna.com>
2019-11-15 17:18:07 +0100

    Refactor multiples

--- hamming_numbers/dynamic_programming.py
+++ hamming_numbers/dynamic_programming.py
@@ -8,22 +8,20 @@ def hamming_generator():
     _h = [h]  # Memoize the sequence hamming numbers
     bases = [2,3,5]
     expos = [0,0,0]
-    next_multiple_of_2 = _h[expos[0]] * bases[0]
-    next_multiple_of_3 = _h[expos[1]] * bases[1]
-    next_multiple_of_5 = _h[expos[2]] * bases[2]
+    multiples = [_h[expos[i]]*bases[i] for i in range(3)]
     while True:
-        h = min(next_multiple_of_2, next_multiple_of_3, next_multiple_of_5)
+        h = min(multiples)
         yield h
         _h.append(h)
-        if h == next_multiple_of_2:
+        if h == multiples[0]:
             expos[0] += 1
-            next_multiple_of_2 = _h[expos[0]] * bases[0]
-        if h == next_multiple_of_3:
+            multiples[0] = _h[expos[0]] * bases[0]
+        if h == multiples[1]:
             expos[1] += 1
-            next_multiple_of_3 = _h[expos[1]] * bases[1]
-        if h == next_multiple_of_5:
+            multiples[1] = _h[expos[1]] * bases[1]
+        if h == multiples[2]:
             expos[2] += 1
-            next_multiple_of_5 = _h[expos[2]] * bases[2]
+            multiples[2] = _h[expos[2]] * bases[2]
 
 
 def hamming(n):
