37888721f81bd9824ef481c1796640a693df9d43
Michael Fichter <michael.fichter@klarna.com>
2019-11-15 17:20:30 +0100

    Reduce duplication with if statements

--- hamming_numbers/dynamic_programming.py
+++ hamming_numbers/dynamic_programming.py
@@ -6,23 +6,15 @@ def hamming_generator():
     h = 1
     yield h
     _h = [h]  # Memoize the sequence hamming numbers
-    bases = [2,3,5]
-    expos = [0,0,0]
-    multiples = [_h[expos[i]]*bases[i] for i in range(3)]
+    bases = [2, 3, 5]
+    expos = [0, 0, 0]
+    multiples = [_h[expos[i]] * bases[i] for i in range(3)]
     while True:
         h = min(multiples)
         yield h
         _h.append(h)
-        if h == multiples[0]:
-            expos[0] += 1
-            multiples[0] = _h[expos[0]] * bases[0]
-        if h == multiples[1]:
-            expos[1] += 1
-            multiples[1] = _h[expos[1]] * bases[1]
-        if h == multiples[2]:
-            expos[2] += 1
-            multiples[2] = _h[expos[2]] * bases[2]
-
+        for i in range(3):
+            expos[i] += int(h == multiples[i])
 
 def hamming(n):
     return list(itertools.islice(hamming_generator(), n))[-1]
